feba114f874c1fb94cabfd41d4798048ed06dfae
mayanksharma2000 <ms9456606@gmail.com>
2024-09-01 23:21:46 +0530

    Create user authentication (login & registration) features and added git ignore.

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -4,7 +4,10 @@ import { food_list } from "../assets/assets";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+
   const [cartItems, setCartItems] = useState({});
+  const url = "http://localhost:4000"
+  const [token,setToken] = useState("");
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
@@ -29,14 +32,23 @@ const StoreContextProvider = (props) => {
     return totalAmount;
   };
 
+  useEffect(()=>{
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));  
+    }
+  },[])
+
   const contextValue = {
     food_list,
     cartItems,
     setCartItems,
     addToCart,
     removeFromCart,
-    getTotalCartAmount
-  };
+    getTotalCartAmount,
+    url,
+    token,
+    setToken
+  }
 
   return (
     <StoreContext.Provider value={contextValue}>
