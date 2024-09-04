ec8b1c6113d76f54e4d7e6c09b0e753237cf8e8b
mayanksharma2000 <ms9456606@gmail.com>
2024-09-05 00:27:43 +0530

    Create Order Update Feature.

--- Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import './PlaceOrder.css';
 import { StoreContext } from '../../context/StoreContext';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 const PlaceOrder = () => {
 
