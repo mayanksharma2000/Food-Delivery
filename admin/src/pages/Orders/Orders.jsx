516a63940a6cef460bef29ddd0983a0845fea326
mayanksharma2000 <ms9456606@gmail.com>
2024-09-04 14:12:18 +0530

    Create Order display page in admin panel.

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,13 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Orders.css'
+import {toast} from "react-toastify"
+import axios from 'axios'
+import {assets} from "../../assets/assets"
 
-const Orders = () => {
+const Orders = ({url}) => {
+
+  const [Orders,setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url+"/api/order/list");
+    if (response.data.success) {
+      setOrders(response.data.data);
+      console.log(response.data.data);
+    }
+    else{
+      toast.error("Error")
+    }
+  }
+
+  useEffect(() =>{
+    fetchAllOrders();
+  },[])
+
   return (
-    <div>
-      
+    <div className='order add'>
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {Orders.map((order,index)=>(
+          <div key={index} className='order-item'>
+              <img src={assets.parcel_icon} alt="" />
+              <div>
+                <p className='order-item-food'>
+                    {order.items.map((item,index)=>{
+                        if (index===order.items.length-1) {
+                          return item.name + " x " +item.quantity
+                        }
+                        else{
+                          return item.name + " x " + item.quantity + ", "
+                        }
+                    })}
+                </p>
+
+                <div className='order-item-name'> {order.address.firstName+" "+order.address.lastName}</div>
+                <div className='order-item-address'>
+                  <p>{order.address.street+", "}</p>
+                  <p>{order.address.city+", "+order.address.state+", "+order.address.country+", "+order.address.zipcode}</p>
+                </div>
+                <p className='order-item-phone'>{order.address.phone}</p>
+              </div>
+              <p>Items : {order.items.length}</p>
+              <p>${order.amount}</p>
+              <select>
+                <option value="Food Processing">Food Processing</option>
+                <option value="Out For Delivery">Out For Delivery</option>
+                <option value="Delivered">Delivered</option>
+              </select>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
-import './Orders.css'
 
 export default Orders
